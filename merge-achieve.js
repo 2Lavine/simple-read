263acd130af9549bc3847179b32a1ce627a3def0
bigideahuang <bigideahuang@foxmail.com>
2024-02-15 22:15:27 +0800

    read

--- merge-achieve.js
+++ merge-achieve.js
@@ -8,10 +8,9 @@ const options = {
 };
 const obsidianImages = require("markdown-it-obsidian-images")(options);
 const md = require("markdown-it")({ html: true }).use(obsidianImages);
-const directoryPath = "./done"; // 替换为目标目录的路径
-const acheievePath = "./achieve";
+const directoryPath = "./course"; // 替换为目标目录的路径
 const resourcesPath = "./_resources"; // 替换为目标目录的路径
-const now = new Date()
+let now = new Date()
   .toLocaleString("zh-CN", {
     year: "numeric",
     month: "numeric",
@@ -21,7 +20,11 @@ const now = new Date()
     second: "2-digit",
   })
   .replace(/[/,:]/g, "-");
+// now = now.slice()
 const outputFilePath = "./output/" + now + ".html"; // 替换为输出文件的路径
+// acheivePath = achieve+now
+// const acheievePath = "./achieve/"+now;
+const acheievePath = directoryPath + "/" + now.split(" ")[0];
 const output = []; // 用于存储所有的 main-content 元素内容
 const nameArray = [];
 function readDirectory(readPath) {
@@ -32,10 +35,11 @@ function readDirectory(readPath) {
     }
     files.forEach((file) => {
       const filePath = path.join(readPath, file);
-      if (fs.statSync(filePath).isDirectory()) {
-        // 如果是目录，则递归读取
-        readDirectory(filePath);
-      } else if (path.extname(file) !== "html") {
+      // if (fs.statSync(filePath).isDirectory()) {
+      //   // 如果是目录，则递归读取
+      //   readDirectory(filePath);
+      // }
+      if (path.extname(file) !== "html") {
         // 如果是 md 文件，则转换为html
         const fileContent = fs.readFileSync(filePath, "utf8");
         const content =
@@ -43,6 +47,10 @@ function readDirectory(readPath) {
           path.basename(file, path.extname(file)) +
           "</h1>" +
           md.render(fileContent);
+        // create folder acheievePath
+        if (!fs.existsSync(acheievePath)) {
+          fs.mkdirSync(acheievePath);
+        }
         fs.rename(filePath, path.join(acheievePath, file), (err) => {
           console.error(err);
         });
@@ -59,9 +67,11 @@ function readDirectory(readPath) {
       // replace _resources/_resources/ to _resources/
       // replace .awebp to .jpg
 
-      const newFileContent = fileContent
-        .replace(/_resources\/_resources\//g, "_resources/")
-        // .replace(/.awebp"/g, ".jpg\"");
+      const newFileContent = fileContent.replace(
+        /_resources\/_resources\//g,
+        "_resources/"
+      );
+      // .replace(/.awebp"/g, ".jpg\"");
       fs.writeFileSync(outputFilePath, newFileContent);
     }
   });
